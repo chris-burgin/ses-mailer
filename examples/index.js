c7cc67bb1397be89972b48ebc75282b74f4985d2
ChrisBurgin <chrisburgin95@gmail.com>
2017-09-25 14:13:27 -0700

    INIT

---
+++ examples/index.js
@@ -0,0 +1,9 @@
+// import modules
+const { isEmpty } = require("..")
+
+// test items
+const arr1 = [0, 1, 2, 3, 4, 5]
+const obj1 = { item1: [0, 1, 2, 3] }
+
+// pass in items
+isEmpty(arr1, obj1)
---
+++ index.js
@@ -0,0 +1,2 @@
+// exposes the entry point for the package
+module.exports = require("./lib/index")
