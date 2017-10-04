93f7502e0987a125edf88ddec2530a338fcc1a3f
ChrisBurgin <chrisburgin95@gmail.com>
2017-10-04 12:02:03 -0400

    First Pass Sending Working

--- examples/index.js
+++ examples/index.js
@@ -7,24 +7,24 @@ const mail = createMail({
   template: "template_1",
 
   // contact information
-  to: ["chrisburgin95@gmail.com", "chris@downloadyouthministry.com"],
-  from: "support@downloadyouthminsitry.com",
+  to: "chrisburgin95@gmail.com, chris@downloadyouthministry.com",
+  from: "chrisburgin95@gmail.com",
   cc: "chrisburgin@downloadyouthministry.com",
   bcc: "hair@downloadyouthministry.com",
+  subject: "This is the email subject",
 
-  // email subject
-  subject: "We have done a thing!",
+  // email content
+  content: {
+    header: "Header of the email",
+    subHeader: "Sub Header of the email",
+    body:
+      "This is the body content of the email, its not much, but its something",
+    footer: "Footer information of the email",
+  },
 })
 
 // send the mail
-sendMail({
-  // send mailer
-  mail: mail,
-
-  // aws ses information
-  key: "key",
-  secret: "secret",
-})
+sendMail(mail)
   // we are done
   .then(() => console.log("finished"))
   // there was an error
